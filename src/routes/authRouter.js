615e2cf408111b219e8e662ed684329a2997ece4
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-08-19 18:33:00 -0700

    Initial: google routes

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,9 +1,14 @@
 const express = require ('express')
 const router = express.Router()
 
+const { isLoggedIn, landingPage, googleAuth, googleCallback, authFailure, protectedPage, logoutUser } = require('../controllers/authController')
 
-
-
+router.get('/', landingPage)
+router.get('/auth/google', googleAuth)
+router.get('/google/callback', googleCallback)
+router.get('/auth/failure', authFailure)
+router.get('/protected', isLoggedIn, protectedPage)
+router.get('/logout', logoutUser)
 
 
 
