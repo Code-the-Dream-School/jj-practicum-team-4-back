4fdddaf6f821518ae436c2269ca18fd7299e3231
MariaZasypkina <maria.zasypkina@gmail.com>
2025-09-16 17:25:13 -0600

    artworkRouter -> endpoint

--- src/routes/artworkRouter.js
+++ src/routes/artworkRouter.js
@@ -1,14 +1,19 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const { isLoggedIn } = require('../middleware/auth')
-const { uploadSingleImage } = require("../controllers/imageController")
-const { searchArtworks, createArtwork, getArtworkById, deleteArtwork } = require("../controllers/artworkController")
+const { isLoggedIn } = require("../middleware/auth");
+const { uploadSingleImage } = require("../controllers/imageController");
+const {
+  searchArtworks,
+  createArtwork,
+  getArtworkById,
+  deleteArtwork,
+} = require("../controllers/artworkController");
 
 // Artwork functionality
-router.get("/", searchArtworks)
-router.post("/", isLoggedIn, uploadSingleImage, createArtwork)
-router.get("/:id", getArtworkById)
-router.delete("/:id", isLoggedIn, deleteArtwork)
+router.get("/", searchArtworks);
+router.post("/", isLoggedIn, uploadSingleImage, createArtwork);
+router.get("/:id", getArtworkById);
+router.delete("/:id", isLoggedIn, deleteArtwork);
 
-module.exports = router
+module.exports = router;
