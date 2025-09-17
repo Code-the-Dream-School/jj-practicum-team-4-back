b86e1e1cdc99b3e84555bb8f3417ed0285447315
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-09-17 14:45:23 -0700

    Update: Added to the router component of artworkRouter

--- src/routes/artworkRouter.js
+++ src/routes/artworkRouter.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const { isLoggedIn } = require('../middleware/auth')
 const { uploadSingleImage } = require("../controllers/imageController")
-const { searchArtworks, createArtwork, getArtworkById, deleteArtwork } = require("../controllers/artworkController")
+const { searchArtworks, createArtwork, getArtworkById, deleteArtwork, getArtworkLikes, addArtworkLike, removeArtworkLike } = require("../controllers/artworkController")
 
 // Artwork Functionality
 router.get("/", searchArtworks)
@@ -12,7 +12,6 @@ router.get("/:id", getArtworkById)
 router.delete("/:id", isLoggedIn, deleteArtwork)
 
 // Like Functionality
-
 router.get("/:id/likes", getArtworkLikes)
 router.post("/:id/likes", isLoggedIn, addArtworkLike)
 router.delete("/:id/likes", isLoggedIn, removeArtworkLike)
